a872b1ffe9afddea8f1e00ad44362a7dc4f88fde
dkarv <punzet.david@gmail.com>
2025-02-18 20:53:41 +0100

    remove log to stdout

--- seafile/homeassistant.sh
+++ seafile/homeassistant.sh
@@ -12,7 +12,7 @@ export INIT_SEAFILE_ADMIN_PASSWORD=$(bashio::config 'admin_password')
 export SEAFILE_SERVER_HOSTNAME=$(bashio::config 'server_hostname')
 export SEAFILE_SERVER_PROTOCOL=$(bashio::config 'server_protocol')
 export SITE_ROOT=$(bashio::config 'server_path')
-export SEAFILE_LOG_TO_STDOUT=true
+#export SEAFILE_LOG_TO_STDOUT=true
 export ENABLE_SEADOC=$(bashio::config 'enable_seadoc')
 
 "/sbin/my_init" "--" "/scripts/enterpoint.sh"
