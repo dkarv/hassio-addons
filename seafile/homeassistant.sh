25752c4dd982739de2925c1d2f3fe5a724356733
dkarv <punzet.david@gmail.com>
2025-02-20 20:46:34 +0100

    write into config file and set env

--- seafile/homeassistant.sh
+++ seafile/homeassistant.sh
@@ -2,8 +2,8 @@
 
 bashio::log.info "Exporting configuration..."
 
-# TODO No idea why directly piping does not work
-# bashio::config 'db_host' > /etc/container_environment/DB_HOST
+CONF_FILE="/opt/seafile/conf/seafile.conf"
+
 echo "$(bashio::config 'db_host')" > /etc/container_environment/DB_HOST
 echo "$(bashio::config 'db_port')" > /etc/container_environment/DB_PORT
 echo "$(bashio::config 'db_user')" > /etc/container_environment/DB_USER
@@ -16,6 +16,16 @@ echo "$(bashio::config 'server_protocol')" > /etc/container_environment/SEAFILE_
 echo "$(bashio::config 'server_path')" > /etc/container_environment/SITE_ROOT
 # echo "true" > /etc/container_environment/SEAFILE_LOG_TO_STDOUT
 echo "$(bashio::config 'enable_seadoc')" > /etc/container_environment/ENABLE_SEADOC
+# TODO set to true if homeassistant in debug mode
+echo "true" > /etc/container_environment/SEAFILE_DOCKER_VERBOSE
+
+bashio::log.info "Adjusting configuration"
+
+mkdir -p /opt/seafile/conf/
+git config --file=$CONF_FILE database.host "$(bashio::config 'db_host')"
+git config --file=$CONF_FILE database.port "$(bashio::config 'db_port')"
+git config --file=$CONF_FILE database.user "$(bashio::config 'db_user')"
+git config --file=$CONF_FILE database.password "$(bashio::config 'db_password')"
 
 bashio::log.info "Starting seafile"
 
