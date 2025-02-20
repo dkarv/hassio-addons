965ce2a6d90aec262bd69b16bb340c38c745309e
David Punzet <d.punzet@testbirds.com>
2025-02-20 17:44:37 +0100

    fix options to env

--- seafile/homeassistant.sh
+++ seafile/homeassistant.sh
@@ -2,23 +2,20 @@
 
 bashio::log.info "Exporting configuration..."
 
-db_host = $(bashio::config 'db_host')
-echo "$db_host"
-echo "$db_host" > /etc/container_environment/DB_HOST
-db_port = $(bashio::config 'db_port')
-echo "$db_port"
-echo "$db_port" > /etc/container_environment/DB_PORT
-#bashio::config 'db_port' > /etc/container_environment/DB_PORT
-#bashio::config 'db_user' > /etc/container_environment/DB_USER
-#bashio::config 'db_password' > /etc/container_environment/DB_PASSWORD
-#bashio::config 'db_name' > /etc/container_environment/SEAFILE_MYSQL_DB_SEAFILE_DB_NAME
-#bashio::config 'admin_email' > /etc/container_environment/INIT_SEAFILE_ADMIN_EMAIL
-#bashio::config 'admin_password' > /etc/container_environment/INIT_SEAFILE_ADMIN_PASSWORD
-#bashio::config 'server_hostname' > /etc/container_environment/SEAFILE_SERVER_HOSTNAME
-#bashio::config 'server_protocol' > /etc/container_environment/SEAFILE_SERVER_PROTOCOL
-#bashio::config 'server_path' > /etc/container_environment/SITE_ROOT
-#echo "true" > /etc/container_environment/SEAFILE_LOG_TO_STDOUT=true
-#bashio::config 'enable_seadoc' > /etc/container_environment/ENABLE_SEADOC
+# TODO No idea why directly piping does not work
+# bashio::config 'db_host' > /etc/container_environment/DB_HOST
+echo "$(bashio::config 'db_host')" > /etc/container_environment/DB_HOST
+echo "$(bashio::config 'db_port')" > /etc/container_environment/DB_PORT
+echo "$(bashio::config 'db_user')" > /etc/container_environment/DB_USER
+echo "$(bashio::config 'db_password')" > /etc/container_environment/DB_PASSWORD
+echo "$(bashio::config 'db_name')" > /etc/container_environment/SEAFILE_MYSQL_DB_SEAFILE_DB_NAME
+echo "$(bashio::config 'admin_email')" > /etc/container_environment/INIT_SEAFILE_ADMIN_EMAIL
+echo "$(bashio::config 'admin_password')" > /etc/container_environment/INIT_SEAFILE_ADMIN_PASSWORD
+echo "$(bashio::config 'server_hostname')" > /etc/container_environment/SEAFILE_SERVER_HOSTNAME
+echo "$(bashio::config 'server_protocol')" > /etc/container_environment/SEAFILE_SERVER_PROTOCOL
+echo "$(bashio::config 'server_path')" > /etc/container_environment/SITE_ROOT
+# echo "true" > /etc/container_environment/SEAFILE_LOG_TO_STDOUT
+echo "$(bashio::config 'enable_seadoc')" > /etc/container_environment/ENABLE_SEADOC
 
 bashio::log.info "Starting seafile"
 
