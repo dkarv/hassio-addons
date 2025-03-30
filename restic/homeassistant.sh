db5d0bcc01444c1f8767338251ebccd6f19085df
dkarv <punzet.david@gmail.com>
2025-03-30 21:45:22 +0200

    first version of restic addon

---
+++ restic/homeassistant.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bashio
+
+bashio::log.info "Starting restic backup"
+
+# Dump databases
+
+
+export RESTIC_PASSWORD=$(bashio::config 'backup_password')
+export RESTIC_REPOSITORY=$(bashio::config 'backup_target')
+export RESTIC_HOST="homeassistant"
+bashio::log.info "Directories: $(bashio::config 'folders')"
+
+restic --help
+restic backup --host "homeassistant" \
+    test
