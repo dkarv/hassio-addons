cedf015f45259c1acf05bf52f9e3c00399dbed8e
dkarv <punzet.david@gmail.com>
2025-03-31 20:16:15 +0000

    test

--- restic/homeassistant.sh
+++ restic/homeassistant.sh
@@ -2,13 +2,13 @@
 
 bashio::log.info "Starting restic backup"
 
-# Dump databases
+# TODO Dump databases
 
 
 export RESTIC_PASSWORD=$(bashio::config 'backup_password')
 export RESTIC_REPOSITORY=$(bashio::config 'backup_target')
 export RESTIC_HOST="homeassistant"
-bashio::log.info "Directories: $(bashio::config 'folders')"
+bashio::log.info "Directories: $(bashio::config 'backup_directories')"
 
 restic --help
 restic backup test
