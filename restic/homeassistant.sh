dc1f30e4f9df00de03a191f833b838adacf0b117
dkarv <punzet.david@gmail.com>
2025-03-31 20:22:04 +0000

    long sleep for debugging

--- restic/homeassistant.sh
+++ restic/homeassistant.sh
@@ -9,6 +9,9 @@ export RESTIC_PASSWORD=$(bashio::config 'backup_password')
 export RESTIC_REPOSITORY=$(bashio::config 'backup_target')
 export RESTIC_HOST="homeassistant"
 bashio::log.info "Directories: $(bashio::config 'backup_directories')"
+# newline separated
+
+sleep 1000000
 
 restic --help
 restic backup test
