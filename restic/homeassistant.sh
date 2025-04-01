468a79c6be769e2b45f75c9344f40ba165573e2d
dkarv <punzet.david@gmail.com>
2025-04-01 19:09:44 +0000

    adjust script and order

--- restic/homeassistant.sh
+++ restic/homeassistant.sh
@@ -2,16 +2,26 @@
 
 bashio::log.info "Starting restic backup"
 
+
+bashio::log.info "Running before_script"
+bash -c "$(bashio::config 'before_script')"
+
+
+bashio::log.info "Setting up SSH key"
 mkdir -p .ssh
 bashio::config 'ssh_key' > .ssh/id_rsa
 chmod 600 .ssh/id_rsa
 
+
+bashio::log.info "Exporting environment variables"
 export RESTIC_PASSWORD=$(bashio::config 'backup_password')
 export RESTIC_REPOSITORY=$(bashio::config 'backup_target')
 export RESTIC_HOST="homeassistant"
 export BACKUP_DIRECTORIES=$(bashio::config 'backup_directories')
 export BACKUP_OPTIONS=$(bashio::config 'backup_options')
 
+
+bashio::log.info "Checking repository"
 if restic cat config >/dev/null 2>&1; then
   bashio::log.info "Found existing restic repository"
 else
@@ -19,8 +29,10 @@ else
   restic init
 fi
 
+
+bashio::log.info "Dumping databases"
 # TODO Dump databases
 
-bash -c "$(bashio::config 'before_script')"
 
+bashio::log.info "Starting backup"
 restic backup $BACKUP_OPTIONS $BACKUP_DIRECTORIES
