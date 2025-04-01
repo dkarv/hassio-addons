8258b1b9a0590bb2b0a268f57afd8e519af02644
dkarv <punzet.david@gmail.com>
2025-04-01 19:32:03 +0000

    fix ssh key export

--- restic/homeassistant.sh
+++ restic/homeassistant.sh
@@ -8,9 +8,10 @@ bash -c "$(bashio::config 'before_script')"
 
 
 bashio::log.info "Setting up SSH key"
-mkdir -p /root/.ssh
-bashio::config 'ssh_key' > /root/.ssh/id_rsa
-chmod 600 .ssh/id_rsa
+mkdir -p /root/.ssh || bashio::log.warn "Failed to create .ssh directory"
+#bashio::config 'ssh_key' > /root/.ssh/id_rsa
+bashio::config 'ssh_key' || bashio::log.warn "Failed to get SSH key"
+chmod 600 /root/.ssh/id_rsa || bashio::log.warn "Failed to set permissions on SSH key"
 
 
 bashio::log.info "Exporting environment variables"
