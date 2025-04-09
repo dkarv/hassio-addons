5107bf2fd14ad2f1e91599a2e317dedeaa6fa009
dkarv <punzet.david@gmail.com>
2025-04-09 20:51:51 +0000

    add back newlines to ssh key

--- restic/homeassistant.sh
+++ restic/homeassistant.sh
@@ -5,7 +5,7 @@ bashio::log.info "Starting restic backup"
 
 bashio::log.info "Setting up SSH key"
 mkdir -p /root/.ssh || bashio::log.warning "Failed to create .ssh directory"
-bashio::config 'ssh_key' > /root/.ssh/id_ed25519 || bashio::log.warning "Failed to get SSH key"
+bashio::config 'ssh_key' | tr '_' '\n' > /root/.ssh/id_ed25519 || bashio::log.warning "Failed to get SSH key"
 chmod 600 /root/.ssh/id_ed25519 || bashio::log.warning "Failed to set permissions on SSH key"
 bashio::config 'ssh_fingerprint' > /root/.ssh/known_hosts || bashio::log.warning "Failed to get SSH fingerprint"
 
